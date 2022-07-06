1f5a8d5f565069290c9297e7f26f04d44ce723fa
alif338 <alifm26072000@gmail.com>
2022-07-06 17:04:41 +0700

    commit pertama

---
+++ pages/components/halaman-pertama.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function HalamanPertama() {
+  return (
+    <div>
+      <Head>
+        <title>Halaman Pertama</title>
+        <meta name="description" content="Halaman Pertama" />
+      </Head>
+      <h1>Halaman Pertama</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        quidem, quisquam.
+      </p>
+    </div>
+  );
+}
