b8a9e2049ce25c280d6e67650750cf8990e137cb
alif338 <alifm26072000@gmail.com>
2022-07-09 19:07:07 +0700

    check db connection 3

--- pages/api/items/add.js
+++ pages/api/items/add.js
@@ -3,6 +3,7 @@ const Item = require('../../../backend/databases/schemas/item_schema');
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
+      console.log(`Add Items: ${process.env.MONGODB_URL}`);
       await Item.create(req.body);
       res.status(200).json({ name: 'Create success', body: req.body });
     } catch(err) {
