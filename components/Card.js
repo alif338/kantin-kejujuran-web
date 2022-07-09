a6c59a2ec8c958a6e779d407d4745b4f56038d0b
alif338 <alifm26072000@gmail.com>
2022-07-09 08:44:38 +0700

    finished transaction page and add simple dropdown

--- components/Card.js
+++ components/Card.js
@@ -4,10 +4,10 @@ export default function Card() {
   return(
     <div className={styles.card}>
       <div className={styles.item_desc}>
-        <img src='assets/images/placeholder.jpg' height={100}/>
+        <img src='assets/images/placeholder.jpg' height={140} style={{borderRadius: "5px"}}/>
         <div className={styles.item_desc_text}>
           <h2>Snack ABC</h2>
-          <p>Added at: <br/> 01/01/1970</p>
+          <p>Added at: <br/> 01/01/1970</p><br/>
           <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
         </div>
       </div>
--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import React, {useState} from 'react';
 
 export default function Header() {
+  const [visible, setVisible] = useState(false);
   return (
     <header className={styles.navbar}>
       <div className={styles.nav_logo}>
@@ -11,18 +13,23 @@ export default function Header() {
       <div className={styles.nav_buttons}>
         <Link href="/">Katalog</Link>
         <div className={styles.spacer}></div>
-        <Link href="/transaction">Balance & Riwayat transaksi</Link>
+        <Link href="/transaction">Balance &amp; Riwayat Transaksi</Link>
       </div>
 
-      <div className={styles.nav_account}>
-        <div className={styles.accountImage}>
-          <img src="/assets/images/download.png" width={30} height={30} />
+      <div className={styles.nav_user}>
+        <div className={styles.nav_account} onClick={() => setVisible(!visible)}>
+          <div className={styles.accountImage}>
+            <img src="/assets/images/download.png" width={30} height={30} />
+          </div>
+          <div className={styles.accountInfo}>
+            <p>12345</p>
+            <p>Muhammad Alif Darmamulia</p>
+          </div>
+          <img src="/assets/icons/VectorDropdown.png" width={15} height={8} />
         </div>
-        <div className={styles.accountInfo}>
-          <p>12345</p>
-          <p>Muhammad Alif Darmamulia</p>
+        <div className={styles.nav_dropdown} style={{display: visible? 'block': 'none'}}>
+          <p>Sign Out</p>
         </div>
-        <img src="/assets/icons/VectorDropdown.png" width={15} height={8} />
       </div>
     </header>
   )
